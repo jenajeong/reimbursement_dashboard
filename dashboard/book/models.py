4fc80ac6a1cb535c7027569dbc614b67e04c0538
jenajeong <skg4660@gmail.com>
2025-09-17 15:10:34 +0900

    [BE]FEAT: 저자, 카테고리, 책 모델 생성 (book/models.py)

--- dashboard/book/models.py
+++ dashboard/book/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100, verbose_name='name')
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    category1 = models.CharField(max_length=100, verbose_name='category1')
+    category2 = models.CharField(max_length=100, verbose_name='category2')
+
+    def __str__(self):
+        return f'{self.category1} > {self.category2}'
+
+class Book(models.Model):
+    
+    BOOK_TYPES = [
+        ('GEN', '일반'),
+        ('PCS', '피스'),
+        ('SCO', '총보'),
+    ]
+
+    # 저자 ID (ForeignKey)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='author_name')
+
+    # 카테고리 ID (ForeignKey)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='category')
+
+    title_korean = models.CharField(max_length=200, verbose_name='title_korean')
+    title_original = models.CharField(max_length=200, verbose_name='title_original', null=True, blank=True)
+    price = models.IntegerField(verbose_name='price')
+    book_type = models.CharField(max_length=3, choices=BOOK_TYPES, default='GEN', verbose_name='book_type')
+    subtitle = models.CharField(max_length=200, verbose_name='subtitle', null=True, blank=True)
+
+    def __str__(self):
+        return self.title_korean
