41c14df308ec0d6a6aed486f0ff66d37e703af7b
jenajeong <skg4660@gmail.com>
2025-09-24 12:35:07 +0900

    [BE]FIX: config의 url 간단히 수정 (config/urls.py)

--- dashboard/config/urls.py
+++ dashboard/config/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/books/', include('book.urls')),
-    path('api/orders/', include('order.urls')),  
+    path('api/', include('book.urls')),
+    path('api/', include('order.urls')),  
       
     # DRF가 제공하는 로그인/로그아웃 뷰를 사용
     path('api-auth/', include('rest_framework.urls')),
