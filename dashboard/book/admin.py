dcd9da356c558bb21165038d9aa601d4631a0a21
jenajeong <skg4660@gmail.com>
2025-09-24 11:40:46 +0900

    [BE]FEAT: 저자 추가 정보에 대한 admin 추가 (book/admin.py)

--- dashboard/book/admin.py
+++ dashboard/book/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Book, Author, Category, PriceHistory
+from .models import Book, Author, Category, PriceHistory, AuthorWork
 
 admin.site.register(Book)
 admin.site.register(Author)
+admin.site.register(AuthorWork)
 admin.site.register(Category)
 admin.site.register(PriceHistory)
