d1fe8d9d3a4982097aa209b9e09c30e687688d79
jenajeong <skg4660@gmail.com>
2025-09-24 11:39:33 +0900

    [BE]FEAT: 저자 목록 조회 및 수정,추가 에 대한 view 구현 (book/views.py)

--- dashboard/book/views.py
+++ dashboard/book/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework import generics
-from .models import Book
-from .serializers import BookListSerializer, BookDetailSerializer
+from .models import Book, Author
+from .permissions import IsAuthorOrAdmin
+from .serializers import BookListSerializer, BookDetailSerializer, AuthorDetailSerializer, AuthorSerializer, AuthorListSerializer
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet, DateFilter
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from datetime import date
 
 
 
@@ -62,3 +65,64 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method == 'GET':
             return [IsAuthenticated()]
         return [IsAdminUser()]
+
+class AuthorListView(generics.ListAPIView):
+    """
+    모든 저자의 기본 정보 목록을 조회하는 뷰입니다.
+    - GET 요청: 'id', 'name', 'date_of_birth', 'contact_number', 
+      'total_books', 'total_songs' 값을 반환합니다.
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorListSerializer
+    permission_classes = [IsAdminUser]
+
+class AuthorCreateView(generics.CreateAPIView):
+    """
+    새로운 저자를 생성하는 뷰입니다.
+    - POST 요청: AuthorSerializer를 사용하여 새로운 저자 데이터를 생성합니다.
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class AuthorDetailView(generics.RetrieveUpdateAPIView):
+    """
+    특정 저자의 상세 정보 조회 및 수정을 위한 뷰입니다.
+    - GET 요청: 기간별 책 정보 및 판매량 포함한 상세 정보를 조회합니다.
+      예시 URL: /authors/1/?start_date=2024-01-01&end_date=2024-12-31
+    - PUT/PATCH 요청: 저자 정보를 수정합니다.
+    """
+    queryset = Author.objects.all()
+    permission_classes = [IsAuthenticated, IsAuthorOrAdmin]
+
+    def get_serializer_class(self):
+        """
+        요청 HTTP 메서드에 따라 다른 시리얼라이저를 반환합니다.
+        조회(GET) 시 AuthorDetailSerializer를 사용하고,
+        수정(PUT/PATCH) 시 AuthorSerializer를 사용합니다.
+        """
+        if self.request.method in ['PUT', 'PATCH']:
+            return AuthorSerializer
+        return AuthorDetailSerializer
+
+    def get_serializer_context(self):
+        """
+        URL 쿼리 파라미터에서 start_date와 end_date를 가져와 
+        시리얼라이저의 context로 전달합니다.
+        """
+        context = super().get_serializer_context()
+        start_date_str = self.request.query_params.get('start_date')
+        end_date_str = self.request.query_params.get('end_date')
+
+        try:
+            if start_date_str:
+                context['start_date'] = date.fromisoformat(start_date_str)
+            if end_date_str:
+                context['end_date'] = date.fromisoformat(end_date_str)
+        except ValueError:
+            # 날짜 형식이 올바르지 않으면 무시하고 다음으로 넘어갑니다.
+            pass
+        
+        return context
+    
