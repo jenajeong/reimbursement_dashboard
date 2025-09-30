838996e615acc12ef13523747c7d43982586a916
jenajeong <skg4660@gmail.com>
2025-09-30 13:31:31 +0900

    [BE]FIX: reimbursement url 수정

--- dashboard/reimbursement/urls.py
+++ dashboard/reimbursement/urls.py
@@ -3,8 +3,8 @@ from .views import BookSalesListView, AuthorSettlementListView
 
 urlpatterns = [
     # 책별 판매 집계 조회 API 엔드포인트
-    path('books/', BookSalesListView.as_view(), name='book-sales'),
+    path('reimbursement/books/', BookSalesListView.as_view(), name='book-sales'),
     
     # 저자별 정산 집계 조회 API 엔드포인트
-    path('authors/', AuthorSettlementListView.as_view(), name='author-settlement'),
+    path('reimbursement/authors/', AuthorSettlementListView.as_view(), name='author-settlement'),
 ]
