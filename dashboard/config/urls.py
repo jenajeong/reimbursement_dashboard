a571b48c539029a1e8777465deb0892576f5c436
jenajeong <skg4660@gmail.com>
2025-09-30 16:42:45 +0900

    [FE]FIX: api라 명시 되어있던 url을 헷갈리지 않도록 수정 (config/urls.py)

--- dashboard/config/urls.py
+++ dashboard/config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('book.urls')),
-    path('api/', include('order.urls')), 
+    path('order/', include('order.urls')),
     path('api/', include('reimbursement.urls')),
       
     # DRF가 제공하는 로그인/로그아웃 뷰를 사용
