a7527d10245bec782ab7d6d33ca19e983ab24bdc
jenajeong <skg4660@gmail.com>
2025-09-30 16:41:42 +0900

    [FE]FIX: api와 실제 url 구별하기 (order/urls.py)

--- dashboard/order/urls.py
+++ dashboard/order/urls.py
@@ -1,15 +1,15 @@
-from .views import OrderListView, OrderCreateView, OrderDetailView
+from .views import OrderListView, OrderCreateView, OrderDetailView, order_list_view
 from django.urls import path
 
 urlpatterns = [
     # 주문 목록을 조회하는 URL (GET 요청)
-    path('orders/', OrderListView.as_view(), name='order-list'),
-    
+    path('api/', OrderListView.as_view(), name='order-list'),
+    path('', order_list_view, name='order_list'),
     # 새로운 주문을 생성하는 URL (POST 요청)
-    path('orders/create/', OrderCreateView.as_view(), name='order-create'),
+    path('api/create/', OrderCreateView.as_view(), name='order-create'),
     
     # 특정 주문의 상세 정보 조회, 수정 및 삭제 URL
     # <int:pk>를 사용하여 특정 주문을 식별합니다.
-    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
+    path('api/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
 ]
 
