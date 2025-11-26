837bca2f23180ec44456711ffae770afc07628e7
jenajeong <skg4660@gmail.com>
2025-11-26 12:26:22 +0900

    [FEAT] 정산 url 설정

--- dashboard/reimbursement/urls.py
+++ dashboard/reimbursement/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
-from .views import BookSalesListView, AuthorSettlementListView, SettlementListView, SettlementDetailView # 뷰 임포트 추가
+from .views import ReimbursementListView,ReimbursementHTMXListView, reimbursement_detail_dummy_view, settlement_toggle_dummy_view, ReimbursementBaseView
 
 urlpatterns = [
-    # 1. 책별 판매 집계 조회 (관리자 전용)
-    path('books/', BookSalesListView.as_view(), name='book-sales'),
-    
-    # 2. 저자별 정산 집계 조회 (본인/관리자)
-    path('authors/', AuthorSettlementListView.as_view(), name='author-settlement'),
-
-    # 3. 정산 기록 목록 조회 및 연도별 일괄 생성 (관리자 전용)
-    path('settlements/', SettlementListView.as_view(), name='settlement-list-create'),
-    
-    # 4. 특정 정산 기록 상태 업데이트 (관리자 전용)
-    path('settlements/<int:pk>/', SettlementDetailView.as_view(), name='settlement-detail-update'),
+    path('', ReimbursementBaseView, name='reimbursement_base'),
+    path('json/list/', ReimbursementListView.as_view(), name='reimbursement-json-list'),
+    path('list/', ReimbursementHTMXListView, name='reimbursement_list'),
+    path('<int:book_id>/detail/', reimbursement_detail_dummy_view, name='reimbursement_detail'),
+    path('<int:book_id>/settle/', settlement_toggle_dummy_view, name='settlement_toggle'),
 ]
